b3c049fddf767770981c08ae498e5aa0ad3f91b4
BarbierTheo <lebarbier.lh@gmail.com>
2025-01-01 05:41:13 +0100

    avancement sur les sorties, ajout du movies.json

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,7 @@ type();
 // CHATGPT
 
 
-fetch("./assets/js/nextMovies.json")
+fetch("./assets/js/movies.json")
   .then((reponse) => reponse.json())
   .then((movies) => {
     for (item of movies["results"]) {
@@ -69,6 +69,31 @@ fetch("./assets/js/nextMovies.json")
       <button type="button" class="cardimg self-center py-2.5 px-5 me-2 mb-2 text-sm font-semibold text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-slate-700">+ d'infos</button>
       </div>
     ` ;
+
+
+    }
+  });
+
+
+  fetch("./assets/js/nextMovies.json")
+  .then((reponse) => reponse.json())
+  .then((movies) => {
+    for (item of movies["results"]) {
+
+          document.querySelector("#card-affiche-section2").innerHTML += `
+       <div class="flex flex-col gap-1 relative">
+           <img class="size-64 object-cover rounded" src="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="">
+           </img>
+         <div class="bg-gradient-to-r from-slate-900 absolute rounded-t flex justify-between w-full">
+            <div class="flex flex-col justify-between p-2">
+              <h3 class="text-xl text-white font-bold">${item.title}</h3>
+              <p class="text-slate-300">${item.release_date}</p>
+            </div>
+            <a href="" class="p-5 text-right">
+              <i class='bx bxs-info-circle text-white text-4xl'></i>
+            </a>
+          </div>
+       </div>` ;
 
 
     }
