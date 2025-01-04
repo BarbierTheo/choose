e72345174a959883774b3d8f1de64a721c7b5294
BarbierTheo <lebarbier.lh@gmail.com>
2025-01-05 00:10:37 +0100

    pagination des films à l'affiche

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,33 +62,224 @@ fetch("./assets/js/movies.json")
     // }
 
 
-    // CARD 
+    // CARDS FILMS A L'AFFICHE
 
     for (item of movies["results"]) {
       i++;
-      document.querySelector("#card-affiche-section").innerHTML += `
-          <label for="my_modal_${i}" class="flex flex-col justify-between gap-2 mb-4">
-             <img src="https://image.tmdb.org/t/p/w500${item.poster_path}" class="hover:shadow-sm flex rounded-lg self-center cursor-pointer md:w-[20rem] lg:w-[17rem] duration-300 ease-in-out ">
-          </label>
-
-         <input type="checkbox" id="my_modal_${i}" class="modal-toggle" />
+      if (i <= 5) {
+        document.getElementById('card-affiche-section1').innerHTML += `
+            <label for="my_modal_${i}" class="flex flex-col">
+            <img src="https://image.tmdb.org/t/p/w500${item.poster_path}"
+              class="hover:shadow-sm flex rounded-lg self-center cursor-pointer lg:w-[17rem] duration-300 ease-in-out lg:h-[26rem]">
+              <p class="mt-1 font-bold text-lg lg:text-left text-center w-56 self-center lg:self-start">${item.title}</p>
+            </label>
+    
+          <input type="checkbox" id="my_modal_${i}" class="modal-toggle" />
           <div class="modal" role="dialog">
             <div class="modal-box w-11/12 max-w-3xl bg-base-100 flex flex-col shadow-2xl">
-               <img src="https://image.tmdb.org/t/p/w500${item.backdrop_path}" class="object-scale rounded mb-4" alt="">
-               <div class="flex justify-between">
-                  <h3 class="text-3xl font-bold text-slate-50">${item.title}</h3>
-                   <div class="flex flex-col text-right">
-                       <p class="text-gray-100 font-medium"><i class='bx bxs-star'></i> ${item.popularity}</p>
-                       <p class="text-gray-200 font-light">${item.release_date}</p>
-                    </div>
-               </div>
+              <img src="https://image.tmdb.org/t/p/w500${item.backdrop_path}" class="object-scale rounded mb-4" alt="">
+              <div class="flex justify-between">
+                <h3 class="text-3xl font-bold text-slate-50">${item.title}</h3>
+                <div class="flex flex-col text-right">
+                  <p class="text-gray-100 font-medium"><i class='bx bxs-star'></i> ${item.popularity}</p>
+                  <p class="text-gray-200 font-light">${item.release_date}</p>
+                </div>
+              </div>
               <p class="text-slate-100">${item.overview}</p>
             </div>
             <label class="modal-backdrop" for="my_modal_${i}">Close</label>
-            </div>
+          </div>
     `;
+      } else {
+        if (i <= 10) {
+          document.getElementById('card-affiche-section2').innerHTML += `
+            <label for="my_modal_${i}" class="flex flex-col">
+            <img src="https://image.tmdb.org/t/p/w500${item.poster_path}"
+              class="hover:shadow-sm flex rounded-lg self-center cursor-pointer lg:w-[17rem] duration-300 ease-in-out lg:h-[26rem]">
+              <p class="mt-1 font-bold text-lg lg:text-left text-center w-56 self-center lg:self-start">${item.title}</p>
+            </label>
+    
+          <input type="checkbox" id="my_modal_${i}" class="modal-toggle" />
+          <div class="modal" role="dialog">
+            <div class="modal-box w-11/12 max-w-3xl bg-base-100 flex flex-col shadow-2xl">
+              <img src="https://image.tmdb.org/t/p/w500${item.backdrop_path}" class="object-scale rounded mb-4" alt="">
+              <div class="flex justify-between">
+                <h3 class="text-3xl font-bold text-slate-50">${item.title}</h3>
+                <div class="flex flex-col text-right">
+                  <p class="text-gray-100 font-medium"><i class='bx bxs-star'></i> ${item.popularity}</p>
+                  <p class="text-gray-200 font-light">${item.release_date}</p>
+                </div>
+              </div>
+              <p class="text-slate-100">${item.overview}</p>
+            </div>
+            <label class="modal-backdrop" for="my_modal_${i}">Close</label>
+          </div>
+      `;
+        } else {
+          if (i <= 15) {
+            document.getElementById('card-affiche-section3').innerHTML += `
+            <label for="my_modal_${i}" class="flex flex-col">
+            <img src="https://image.tmdb.org/t/p/w500${item.poster_path}"
+              class="hover:shadow-sm flex rounded-lg self-center cursor-pointer lg:w-[17rem] duration-300 ease-in-out lg:h-[26rem]">
+              <p class="mt-1 font-bold text-lg lg:text-left text-center w-56 self-center lg:self-start">${item.title}</p>
+            </label>
+    
+          <input type="checkbox" id="my_modal_${i}" class="modal-toggle" />
+          <div class="modal" role="dialog">
+            <div class="modal-box w-11/12 max-w-3xl bg-base-100 flex flex-col shadow-2xl">
+              <img src="https://image.tmdb.org/t/p/w500${item.backdrop_path}" class="object-scale rounded mb-4" alt="">
+              <div class="flex justify-between">
+                <h3 class="text-3xl font-bold text-slate-50">${item.title}</h3>
+                <div class="flex flex-col text-right">
+                  <p class="text-gray-100 font-medium"><i class='bx bxs-star'></i> ${item.popularity}</p>
+                  <p class="text-gray-200 font-light">${item.release_date}</p>
+                </div>
+              </div>
+              <p class="text-slate-100">${item.overview}</p>
+            </div>
+            <label class="modal-backdrop" for="my_modal_${i}">Close</label>
+          </div>
+        `;
+          } else {
+            if (i <= 20) {
+              document.getElementById('card-affiche-section4').innerHTML += `
+            <label for="my_modal_${i}" class="flex flex-col">
+            <img src="https://image.tmdb.org/t/p/w500${item.poster_path}"
+              class="hover:shadow-sm flex rounded-lg self-center cursor-pointer lg:w-[17rem] duration-300 ease-in-out lg:h-[26rem]">
+              <p class="mt-1 font-bold text-lg lg:text-left text-center w-56 self-center lg:self-start">${item.title}</p>
+            </label>
+  
+        <input type="checkbox" id="my_modal_${i}" class="modal-toggle" />
+        <div class="modal" role="dialog">
+          <div class="modal-box w-11/12 max-w-3xl bg-base-100 flex flex-col shadow-2xl">
+            <img src="https://image.tmdb.org/t/p/w500${item.backdrop_path}" class="object-scale rounded mb-4" alt="">
+            <div class="flex justify-between">
+              <h3 class="text-3xl font-bold text-slate-50">${item.title}</h3>
+              <div class="flex flex-col text-right">
+                <p class="text-gray-100 font-medium"><i class='bx bxs-star'></i> ${item.popularity}</p>
+                <p class="text-gray-200 font-light">${item.release_date}</p>
+              </div>
+            </div>
+            <p class="text-slate-100">${item.overview}</p>
+          </div>
+          <label class="modal-backdrop" for="my_modal_${i}">Close</label>
+        </div>
+      `;
+            }
+          }
+        }
+
+      }
     }
   });
+
+
+// PAGINATION BTN 1
+
+  const toggleaffiche1 = document.getElementById("buttoncardaffiche1");
+  function showaffiche1() {
+
+    document.getElementById("card-affiche-section4").classList.remove('flex')
+    document.getElementById("card-affiche-section4").classList.add('hidden')
+
+    document.getElementById("card-affiche-section3").classList.remove('flex')
+    document.getElementById("card-affiche-section3").classList.add('hidden')
+    
+    document.getElementById("card-affiche-section2").classList.remove('flex')
+    document.getElementById("card-affiche-section2").classList.add('hidden')
+
+    document.getElementById("card-affiche-section1").classList.remove('hidden')
+    document.getElementById("card-affiche-section1").classList.add('flex')
+
+    document.getElementById("buttoncardaffiche1").classList.add('btn-active')
+    document.getElementById("buttoncardaffiche2").classList.remove('btn-active')
+    document.getElementById("buttoncardaffiche3").classList.remove('btn-active')
+    document.getElementById("buttoncardaffiche4").classList.remove('btn-active')
+
+}
+toggleaffiche1.addEventListener("click", showaffiche1)
+
+// PAGINATION BTN 2
+
+const toggleaffiche2 = document.getElementById("buttoncardaffiche2");
+function showaffiche2() {
+
+  document.getElementById("card-affiche-section4").classList.remove('flex')
+  document.getElementById("card-affiche-section4").classList.add('hidden')
+
+  document.getElementById("card-affiche-section3").classList.remove('flex')
+  document.getElementById("card-affiche-section3").classList.add('hidden')
+  
+  document.getElementById("card-affiche-section1").classList.remove('flex')
+  document.getElementById("card-affiche-section1").classList.add('hidden')
+
+  document.getElementById("card-affiche-section2").classList.remove('hidden')
+  document.getElementById("card-affiche-section2").classList.add('flex')
+  
+  document.getElementById("buttoncardaffiche2").classList.add('btn-active')
+  document.getElementById("buttoncardaffiche1").classList.remove('btn-active')
+  document.getElementById("buttoncardaffiche3").classList.remove('btn-active')
+  document.getElementById("buttoncardaffiche4").classList.remove('btn-active')
+
+}
+toggleaffiche2.addEventListener("click", showaffiche2)
+
+// PAGINATION BTN 3
+
+const toggleaffiche3 = document.getElementById("buttoncardaffiche3");
+function showaffiche3() {
+
+  document.getElementById("card-affiche-section4").classList.remove('flex')
+  document.getElementById("card-affiche-section4").classList.add('hidden')
+
+  document.getElementById("card-affiche-section2").classList.remove('flex')
+  document.getElementById("card-affiche-section2").classList.add('hidden')
+  
+  document.getElementById("card-affiche-section1").classList.remove('flex')
+  document.getElementById("card-affiche-section1").classList.add('hidden')
+
+  document.getElementById("card-affiche-section3").classList.remove('hidden')
+  document.getElementById("card-affiche-section3").classList.add('flex')
+  
+  document.getElementById("buttoncardaffiche3").classList.add('btn-active')
+  document.getElementById("buttoncardaffiche2").classList.remove('btn-active')
+  document.getElementById("buttoncardaffiche1").classList.remove('btn-active')
+  document.getElementById("buttoncardaffiche4").classList.remove('btn-active')
+
+}
+toggleaffiche3.addEventListener("click", showaffiche3)
+
+// PAGINATION BTN 4
+
+const toggleaffiche4 = document.getElementById("buttoncardaffiche4");
+function showaffiche4() {
+
+  document.getElementById("card-affiche-section3").classList.remove('flex')
+  document.getElementById("card-affiche-section3").classList.add('hidden')
+
+  document.getElementById("card-affiche-section2").classList.remove('flex')
+  document.getElementById("card-affiche-section2").classList.add('hidden')
+  
+  document.getElementById("card-affiche-section1").classList.remove('flex')
+  document.getElementById("card-affiche-section1").classList.add('hidden')
+
+  document.getElementById("card-affiche-section4").classList.remove('hidden')
+  document.getElementById("card-affiche-section4").classList.add('flex')
+  
+  document.getElementById("buttoncardaffiche4").classList.add('btn-active')
+  document.getElementById("buttoncardaffiche2").classList.remove('btn-active')
+  document.getElementById("buttoncardaffiche1").classList.remove('btn-active')
+  document.getElementById("buttoncardaffiche3").classList.remove('btn-active')
+
+}
+toggleaffiche4.addEventListener("click", showaffiche4)
+
+
+
+
+
+
+// AFFICHAGE DES CARTES PROCHAINES SORTIES
 
 let a = 0;
 fetch("./assets/js/nextMovies.json")
@@ -96,7 +287,13 @@ fetch("./assets/js/nextMovies.json")
   .then((movies) => {
     for (item of movies["results"]) {
       a = a - 1;
-      document.querySelector("#card-affiche-section2").innerHTML += `
+
+      if (a>=-10) {
+
+
+      }
+
+      document.querySelector("#card-sorties-section2").innerHTML += `
        <div class="flex flex-col gap-1 relative">
            <img class="size-64 object-cover rounded" src="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="">
            </img>
